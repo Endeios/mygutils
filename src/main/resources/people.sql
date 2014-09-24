9fbe6550698fd2fe24e509ea3501422d558463d4
endeios@gmail.com <endeios@gmail.com>
2014-09-24 10:12:52 +0200

    added sqlite data

---
+++ src/main/resources/people.sql
@@ -0,0 +1,7 @@
+create table person (id autoincrement integer primary key ,name text, description text,a_number integer);
+
+insert into person (name,description,a_number) values ('Mr.Potato', 'the Potato',   10);
+insert into person (name,description,a_number) values ('Mr.Tomato', 'the Tomato',   12);
+insert into person (name,description,a_number) values ('Mr.Carrot', 'the Carrot',   23);
+insert into person (name,description,a_number) values ('Mr.Pear'  , 'the Pear'  ,   65);
+insert into person (name,description,a_number) values ('Mr.Apricot', 'the Apricot', 34);
