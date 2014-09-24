14b6a96c0ad9db8e21ed7952b4e70a86ad4aa870
endeios@gmail.com <endeios@gmail.com>
2014-09-24 12:43:09 +0200

    added example for list factory

--- src/main/groovy/GroovyFxExamples.groovy
+++ src/main/groovy/GroovyFxExamples.groovy
@@ -4,12 +4,17 @@ import static groovyx.javafx.GroovyFX.start
 import groovy.sql.Sql
 import javafx.collections.FXCollections
 import javafx.scene.control.*
-
+import org.apache.commons.lang3.StringUtils
 def classLoader = Thread.currentThread().getContextClassLoader()
 
 datafixUri = classLoader.findResource("people.db")
 people = Sql.newInstance("jdbc:sqlite:${datafixUri.getFile()}",'','','org.sqlite.JDBC')
 
+csv = 
+"""a;b;c
+1;2;3
+4;5;6
+"""
 
 start{
     /**
@@ -18,8 +23,7 @@ start{
     def queryTextField;
     def queryButton;
     def queryTableView;
-    def headersTextField;
-    def dataTextField;
+    def csvTextArea;
     def listButton;
     def listTableView;
     stage(title:"MyFactories example",visible:true){
@@ -56,15 +60,32 @@ start{
                 tab(text:"List of Values"){
                     vbox(padding:20){
                         //label
-                        label(text:"Headers, comma separated")
-                        //headers textfield
-                        headersTextField=textField(prefColumnCount:20,height:40)
-                        //label
-                        label(text:"Data,in ordered lines, json style")
+                        label(text:"Header and Data,in ordered lines, csv(semicolon ';' separated ) style")
                         //data textfield
-                        dataTextField=textField(prefColumnCount:20,height:40)
+                        dataTextField=textArea(prefColumnCount:20,height:140,text:csv)
                         //execute button
-                        listButton=button(text:"List data")
+                        listButton=button(text:"List data",onAction:{event->
+                            def rawData = dataTextField.text
+                            def lines = StringUtils.split(rawData,"\n")
+                            def headers = StringUtils.split(lines.head(),";")
+                            def cols = listTableView.getColumns()
+                            cols.removeAll(cols)
+                            if(headers!=null){
+                                headers.eachWithIndex{header,index->
+                                    def col = new TableColumn("$header")
+                                    col.setCellValueFactory(new ListResultValueFactory(index))
+                                    cols.add(col)
+                                }
+                            }
+                            
+                           def list =lines.tail().collect{line->
+                                StringUtils.split(line,";")
+                           }
+
+                           def data = FXCollections.observableArrayList(list)
+                           listTableView.items=data
+
+                        })
                         //tableView
                         listTableView=tableView()
                     }
